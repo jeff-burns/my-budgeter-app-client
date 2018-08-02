47d2ad3c04c0d4d869f7b2e27eb9eb45497a9851
jeff-burns <1jeffreyburns@gmail.com>
2018-08-01 21:54:03 -0600

    added dropdown menu to account name in top corner with a delete and signout

--- src/AcctName/AcctName.js
+++ src/AcctName/AcctName.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap';
 // import InputForm from '../InputForm/InputForm'
 
 
@@ -7,7 +7,16 @@ const AcctName = (props) => {
   return (
     <Row>
         <Col sm={{ size: 2, offset: 6 }} md={{ size: 2, offset: 8 }} lg={{ size: 2, offset: 9 }}>
-            <p className="text-white">{props.accountName}</p>
+            <Dropdown isOpen={props.dropdownOpen} toggle={props.toggleEmail}>
+                <DropdownToggle  caret>
+                    {props.accountName}
+                </DropdownToggle>
+                <DropdownMenu right>
+                    <DropdownItem>Delete User Data</DropdownItem>
+                    <DropdownItem divider/>
+                    <DropdownItem>Sign Out</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
         </Col>
     </Row>
   )
--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-// import { Col } from 'reactstrap';
+import { Row, Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap';
 import './App.css';
 
 import DropDown from './components/DropDown/DropDown';
 import CreditPage from './components/CreditPage/CreditPage';
 import DebitPage from './components/DebitPage/DebitPage';
 import LogIn from './components/LogIn/LogIn';
-import AcctName from './AcctName/AcctName';
+// import AcctName from './AcctName/AcctName';
 
 class App extends Component {
   constructor(props) {
@@ -17,8 +17,11 @@ class App extends Component {
       this.handleChange = this.handleChange.bind(this);
       this.handleLogIn = this.handleLogIn.bind(this);
       this.handleCreateAccount = this.handleCreateAccount.bind(this);
+      this.toggleEmail = this.toggleEmail.bind(this);
+
       this.state = {
-        dropdownOpen: false,
+        dropDownOpen: false,
+        acctToggleOpen: false,
         creditOpen: false,
         debitOpen: false,
         acctName: "",
@@ -27,9 +30,13 @@ class App extends Component {
       };
     }
 
+//componentDidMount() {
+//  fetch users and passwords and setState 
+//}
+
     toggleAccount() {
       this.setState({
-        dropdownOpen: !this.state.dropdownOpen,
+        acctToggleOpen: !this.state.acctToggleOpen,
         debitOpen: false,
         creditOpen: false,
       });
@@ -39,7 +46,7 @@ class App extends Component {
       this.setState({
         creditOpen: !this.state.creditOpen,
         debitOpen: false,
-        dropdownOpen: false
+        acctToggleOpen: false
       });
     }
 
@@ -47,7 +54,7 @@ class App extends Component {
       this.setState({
         debitOpen: !this.state.debitOpen,
         creditOpen: false,
-        dropdownOpen: false
+        acctToggleOpen: false
       });
     }
 
@@ -56,34 +63,62 @@ class App extends Component {
       this.setState({
         acctName: event.target.value
       })
+      //also save password here?
     }
 
     handleLogIn(event) {
       event.preventDefault();
       this.setState({
         emailSubmitted: true,
-        dropdownOpen: false,
+        acctToggleOpen: false,
         debitOpen: true
       })
+      //check state where users are for the user and password
+      //THEN fetch that user data from another table in the db and fill card data with it
     }
 
     handleCreateAccount(event) {
       event.preventDefault();
       this.setState({
         emailSubmitted: true,
-        dropdownOpen: false,
+        acctToggleOpen: false,
         creditOpen: true
       })
+      //fetch post with email and password to users table in db
     }
+
+    toggleEmail() {
+      this.setState(prevState => ({
+        dropdownOpen: !prevState.dropdownOpen
+      }));
+    }
+
+    //handle credits and debits -
+      //do math for adding and subtracting and the averages needed
+      //post or put this data to the user data table
+      //maybe make a check on current date to the previous data dates and if it's in the same month, do a put, if not do a post for a new month
+      //on re-render it should update this data because of the componentDidMount fetch 
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           { (this.state.emailSubmitted) ?
-            <AcctName 
-              accountName={this.state.acctName} 
-              /> : null }
+            <Row>
+              <Col sm={{ size: 2, offset: 6 }} md={{ size: 2, offset: 8 }} lg={{ size: 2, offset: 9 }}>
+                  <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggleEmail}>
+                      <DropdownToggle  caret>
+                          {this.state.acctName}
+                      </DropdownToggle>
+                      <DropdownMenu right>
+                          <DropdownItem>Delete User Data</DropdownItem>
+                          <DropdownItem divider/>
+                          <DropdownItem>Sign Out</DropdownItem>
+                      </DropdownMenu>
+                  </Dropdown>
+              </Col>
+            </Row>
+              : null }
           <h1 className="App-title">MyBudgeter</h1>
           <DropDown 
               toggleAccount={this.toggleAccount}
@@ -93,7 +128,7 @@ class App extends Component {
         </header>
         
         <main>
-          { (this.state.dropdownOpen) ? 
+          { (this.state.acctToggleOpen) ? 
             <LogIn 
               acctName={this.handleChange}
               logIn={this.handleLogIn}
@@ -110,3 +145,9 @@ class App extends Component {
 }
 
 export default App;
+
+//<AcctName 
+// accountName={this.state.acctName}
+// isOpen={this.state.dropDownOpen}
+// toggle={this.toggleEmail} 
+// />
